21484740ac102b47f9106defb7d82f01cc26e32c
perlancar <perlancar@gmail.com>
2014-12-28 13:52:56 +0000

    Import of PERLANCAR/App-WeaverUtils-0.02.tar.gz from CPAN.
    gitpan-cpan-distribution: App-WeaverUtils
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         PERLANCAR/App-WeaverUtils-0.02.tar.gz
    gitpan-cpan-author:       PERLANCAR
    gitpan-cpan-maturity:     released

--- lib/App/WeaverUtils.pm
+++ lib/App/WeaverUtils.pm
@@ -1,63 +1,9 @@
 package App::WeaverUtils;
 
-our $DATE = '2014-12-14'; # DATE
-our $VERSION = '0.01'; # VERSION
+our $DATE = '2014-12-28'; # DATE
+our $VERSION = '0.02'; # VERSION
 
 use 5.010001;
-use strict;
-use warnings;
-
-our $_complete_stuff = sub {
-    require Complete::Module;
-    my $which = shift;
-    my %args = @_;
-
-    my $word = $args{word} // '';
-
-    # convenience: allow Foo/Bar.{pm,pod,pmc}
-    $word =~ s/\.(pm|pmc|pod)\z//;
-
-    # compromise, if word doesn't contain :: we use the safer separator /, but
-    # if already contains '::' we use '::' (but this means in bash user needs to
-    # use quote (' or ") to make completion behave as expected since : is a word
-    # break character in bash/readline.
-    my $sep = $word =~ /::/ ? '::' : '/';
-    $word =~ s/\W+/::/g;
-    my $ns_prefix    = 'Pod::Weaver::'.
-        ($which eq 'bundle' ? 'PluginBundle' :
-             $which eq 'plugin' ? 'Plugin' :
-                 $which eq 'role' ? 'Role' :
-                     $which eq 'section' ? 'Section' : ''
-                 ).'::';
-
-    {
-        words => Complete::Module::complete_module(
-            word      => $word,
-            ns_prefix => $ns_prefix,
-            find_pmc  => 0,
-            find_pod  => 0,
-            separator => $sep,
-            ci        => 1, # convenience
-        ),
-        path_sep => $sep,
-    };
-};
-
-our $_complete_bundle = sub {
-    $_complete_stuff->('bundle', @_);
-};
-
-our $_complete_plugin = sub {
-    $_complete_stuff->('plugin', @_);
-};
-
-our $_complete_role = sub {
-    $_complete_stuff->('role', @_);
-};
-
-our $_complete_section = sub {
-    $_complete_stuff->('section', @_);
-};
 
 1;
 # ABSTRACT: Collection of CLI utilities for Pod::Weaver
@@ -74,7 +20,7 @@ App::WeaverUtils - Collection of CLI utilities for Pod::Weaver
 
 =head1 VERSION
 
-This document describes version 0.01 of App::WeaverUtils (from Perl distribution App-WeaverUtils), released on 2014-12-14.
+This document describes version 0.02 of App::WeaverUtils (from Perl distribution App-WeaverUtils), released on 2014-12-28.
 
 =head1 DESCRIPTION
 
